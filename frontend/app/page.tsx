afa74b85b9ac19f14a816d62609a688b1dba6bc1
jakubstribrny <kubastribrny1@seznam.cz>
2025-06-16 16:35:04 +0200

    fix - className instead of class - we are not in html lol

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
-        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">Project <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-500 from-green-400">LCMR01</span></h1>
+        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">Project <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-500 from-green-400">LCMR01</span></h1>
         <p className="text-lg font-normal text-gray-300 lg:text-xl dark:text-gray-400">Low-Cost Camera</p>
         <p className="text-lg font-normal text-gray-300 lg:text-xl dark:text-gray-400">FastAPI & Next.js</p>
 
